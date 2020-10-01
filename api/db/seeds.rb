0164534edf47d603a9447c74f12af82c5a66ce5f
PhilNorman2 <31697968+PhilNorman2@users.noreply.github.com>
2020-10-01 17:11:43 -0400

    added rails swagger (rswag) api testing and documentation (#10)
    * added rails swagger (rswag) api testing and documentation
    
    * updated database seed cases and README.md
    
    * minor gramatical change

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -19,9 +19,15 @@ IncidentSeverity.create([{ description: 'No Apparent Injury'},
                      { description: 'Suspected Serious Injury'},
                      { description: 'Fatal Injury'}])
 
-Report.create([{ lat: 39.98460665, long: -82.91922267, incident_type_id: 1 },
-               { lat: 39.95800777, long: -82.97354855, incident_type_id: 2 },
-               { lat: 39.86176882, long: -83.17015774, incident_type_id: 3 },
-               { lat: 39.920211, long: -82.832104, incident_type_id: 4 } ])
+Report.create([{ lat: 39.98460665, long: -82.91922267, incident_datetime: '2020-09-30T19:23:15.841Z',
+                 incident_text: 'lorem ipsum 1 ...', incident_type_id: 1, incident_severity_id: 1 },
+               { lat: 39.95800777, long: -82.97354855, incident_datetime: '2020-09-30T19:23:15.841Z',
+                 incident_text: 'lorem ipsum 2 ...', incident_type_id: 2, incident_severity_id: 2 },
+               { lat: 39.86176882, long: -83.17015774, incident_datetime: '2020-09-30T19:23:15.841Z',
+                 incident_text: 'lorem ipsum 3 ...',incident_type_id: 3, incident_severity_id: 3 },
+               { lat: 39.920211, long: -82.832104, incident_datetime: '2020-09-30T19:23:15.841Z',
+                 incident_text: 'lorem ipsum 4 ...', incident_type_id: 4, incident_severity_id: 4 },
+               { lat: 39.920211, long: -82.832104, incident_datetime: '2020-09-30',
+                 incident_text: 'lorem ipsum 5 ...', incident_type_id: 4, incident_severity_id: 5 }])
 
 
